a0699848f5db8f7003900a57ac6edf44de1c3a5d
Hafizh Siregar <shishiroobotan@gmail.com>
2025-11-16 13:04:04 +0700

    Add files via upload

---
+++ tentang.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def show():
+    st.title("ℹ️ Tentang Aplikasi NewsScope")
+    st.write("""
+    NewsScope adalah aplikasi pencarian berita Indonesia yang menampilkan hasil secara cepat berdasarkan kata kunci.
+    Aplikasi ini dibangun menggunakan Python dan Streamlit
+
+    **Fitur:**
+    - Cari berita berdasarkan topik tertentu.
+    - Menampilkan sumber dan tanggal publikasi.
+    Dibuat oleh: Muhammad al hafizh siregar
+    """)
