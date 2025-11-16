b8cced4002e11af56d02a845e9f89132a9413581
Hafizh Siregar <shishiroobotan@gmail.com>
2025-11-16 12:57:20 +0700

    Update tentang.py

--- tentang.py
+++ tentang.py
@@ -4,10 +4,11 @@ def show():
     st.title("ℹ️ Tentang Aplikasi NewsScope")
     st.write("""
     **NewsScope** adalah aplikasi pencari berita terkini dari berbagai sumber terpercaya.
-    Dibuat menggunakan **Python + Streamlit**, aplikasi ini memanfaatkan **NewsAPI** untuk mengambil berita berdasarkan kategori dan kata kunci.
+    Dibuat menggunakan **Python + Streamlit**, aplikasi ini memanfaatkan **NewsAPI** untuk mengambil berita berdasarkan kata kunci.
 
     **Fitur:**
     - Cari berita berdasarkan topik tertentu.
     - Menampilkan sumber dan tanggal publikasi.
     Dibuat oleh: Muhammad al hafizh siregar
     """)
+
